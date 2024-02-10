f7fa40439bca144378328fe3d699f64cc0597137
Vlad <mackva34@gmail.com>
2024-02-10 18:30:28 +0100

    create homework projekt

--- HomeWork-4/ViewController.swift
+++ HomeWork-4/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        if let profileVC = segue.source as? ProfileViewController {
+            profileVC.userName = userNameTextField.text
+            userNameTextField.text = ""
+            passwordTextField.text = ""
+        }
     }
-
-
+    
+    @IBAction func showUserName() {
+        showAlert(withTitle: "Oops!", andMessage: "Your User Name is 11 \u{263A}")
+    }
+    
+    @IBAction func showPassword() {
+        showAlert(withTitle: "Oops!", andMessage: "Your Password is 11 \u{263A}")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let profileVC = segue.destination as? ProfileViewController {
+            profileVC.userName = userNameTextField.text
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == "11", passwordTextField.text == "11" else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login or password"
+            )
+            return false
+        }
+        return true
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.userNameTextField.text = ""
+            self.passwordTextField.text = ""
+        })
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
