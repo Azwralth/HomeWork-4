743016e699000a7dbce89b6f1889ff25a1edbee6
Vlad <mackva34@gmail.com>
2024-02-10 19:02:43 +0100

    swapped methods

--- HomeWork-4/ViewController.swift
+++ HomeWork-4/ViewController.swift
@@ -12,22 +12,6 @@ final class ViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    @IBAction func unwind(for segue: UIStoryboardSegue) {
-        if let profileVC = segue.source as? ProfileViewController {
-            profileVC.userName = userNameTextField.text
-            userNameTextField.text = ""
-            passwordTextField.text = ""
-        }
-    }
-    
-    @IBAction func showUserName() {
-        showAlert(withTitle: "Oops!", andMessage: "Your User Name is 11 \u{263A}")
-    }
-    
-    @IBAction func showPassword() {
-        showAlert(withTitle: "Oops!", andMessage: "Your Password is 11 \u{263A}")
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let profileVC = segue.destination as? ProfileViewController {
             profileVC.userName = userNameTextField.text
@@ -48,6 +32,22 @@ final class ViewController: UIViewController {
             return false
         }
         return true
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        if let profileVC = segue.source as? ProfileViewController {
+            profileVC.userName = userNameTextField.text
+            userNameTextField.text = ""
+            passwordTextField.text = ""
+        }
+    }
+    
+    @IBAction func showUserName() {
+        showAlert(withTitle: "Oops!", andMessage: "Your User Name is 11 \u{263A}")
+    }
+    
+    @IBAction func showPassword() {
+        showAlert(withTitle: "Oops!", andMessage: "Your Password is 11 \u{263A}")
     }
     
     private func showAlert(withTitle title: String, andMessage message: String) {
